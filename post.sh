574067d3576507b19d191617c5f2fe20498a8510
Anantha Kumaran <ananthakumaran@gmail.com>
2010-10-13 22:24:49 +0530

    clean the script

--- post.sh
+++ post.sh
@@ -1,1 +1,16 @@
-emacs ./_posts/$2/`date +%F`-$1.markdown &
+file=./_posts/$2/`date +%F`-$1.markdown
+if [ ! -d ./_posts/$2 ]
+then
+    mkdir ./_posts/$2;
+fi
+cat > $file  << EOF
+---
+layout: post
+title:
+header:
+meta_keywords:
+meta_descripton:
+---
+EOF
+
+emacsclient ./_posts/$2/`date +%F`-$1.markdown -n
